dc51125ab611e4d9cd756b451ca02fb6732babcb
DaffaZimraan <daffazimraan12@gmail.com>
2024-11-17 13:59:34 +0700

    Modify client.py from Task 2

--- Task 3 - RSA Implementation/client.py
+++ Task 3 - RSA Implementation/client.py
@@ -1,15 +1,44 @@
 import socket
 from des import encrypt, decrypt  
+from rsa import generate_keys, encrypt as rsa_encrypt, decrypt as rsa_decrypt
 
 def client_program():
     host = socket.gethostname()  
-    port = 5000  
+    port = 5000 
+    pka_host = socket.gethostname()
+    pka_port = 5001 
 
+    client_private_key, client_public_key = generate_keys()
+    
+    # Print Server keys
+    print("Client Keys:")
+    print(f"Public Key (e, n): {client_public_key}")
+    print(f"Private Key (d, n): {client_private_key}\n")
+    
+    pka_socket = socket.socket()
+    pka_socket.connect((pka_host, pka_port))
+    
+    #If already connected to pka, send the client's public key to pka and print the log
+    pka_socket.send(f"{client_public_key}".encode())  # Send as a string
+    print("Client Public Key sent to PKA.")
+    
+    pka_public_key = pka_socket.recv(1024).decode()
+    print(f"Received PKA Public Key: {pka_public_key}")
+    pka_socket.close()
+    
+    # Request server public key from PKA
+    pka_socket = socket.socket()
+    pka_socket.connect((pka_host, pka_port))
+    pka_socket.send("REQUEST:SERVER".encode())  # Request server public key
+    server_public_key = pka_socket.recv(1024).decode()
+    print(f"Received Server Public Key from PKA: {server_public_key}")
+    pka_socket.close()
+    
     client_socket = socket.socket()
     client_socket.connect((host, port))
 
     des_key = client_socket.recv(1024)  
-    print(f"Received generated key: {des_key.hex()}")
+    print(f"Received generated DES key: {des_key.hex()}")
 
     message = input("Type your message here (exit): ")
 
